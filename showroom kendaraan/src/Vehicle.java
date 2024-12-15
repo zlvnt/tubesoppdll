e3ac789eb79764c700af3598f723f3ed6d1d832f
zlvnt <zlvyynnnn@gmail.com>
2024-12-15 08:46:13 +0700

    Update Vehicle.java
    3

--- showroom kendaraan/src/Vehicle.java
+++ showroom kendaraan/src/Vehicle.java
@@ -1,14 +1,14 @@
 public class Vehicle {
     private String brand;
     private String model;
-    private int price;
+    private int harga;
     private int stockQuantity;
 
     // Constructor
-    public Vehicle(String brand, String model, int price, int stockQuantity) {
+    public Vehicle(String brand, String model, int harga, int stockQuantity) {
         this.brand = brand;
         this.model = model;
-        this.price = price;
+        this.harga = harga;
         this.stockQuantity = stockQuantity;
     }
 
@@ -29,16 +29,12 @@ public class Vehicle {
         this.model = model;
     }
 
-    public int getPrice() {
-        return price;
+    public int getHarga() {
+        return harga;
     }
 
-    public void setPrice(int price) {
-        if (price < 0) {
-            System.out.println("Harga tidak boleh negatif!");
-        } else {
-            this.price = price;
-        }
+    public void setHarga(int harga) {
+        this.harga = harga;
     }
 
     public int getStockQuantity() {
@@ -46,15 +42,11 @@ public class Vehicle {
     }
 
     public void setStockQuantity(int stockQuantity) {
-        if (stockQuantity < 0) {
-            System.out.println("Stok tidak boleh negatif!");
-        } else {
-            this.stockQuantity = stockQuantity;
-        }
+        this.stockQuantity = stockQuantity;
     }
 
     // Display basic vehicle information
     public String toString() {
-        return "Brand: " + brand + ", Model: " + model + ", Price: " + price + ", Stock: " + stockQuantity;
+        return "Brand: " + brand + ", Model: " + model + ", Harga: " + harga + ", Stock: " + stockQuantity;
     }
 }
