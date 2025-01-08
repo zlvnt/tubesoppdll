3f1d448c156bfef50d398d5c5fa0fde3dd856103
zlvnt <zlvyynnnn@gmail.com>
2025-01-08 15:08:13 +0700

    Update VehicleTest.java

--- showroom kendaraan/src/VehicleTest.java
+++ showroom kendaraan/src/VehicleTest.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class VehicleTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        kontrolshowroom controller = new kontrolshowroom(scanner);
-        controller.start();
-        scanner.close();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            kontrolshowroom controller = new kontrolshowroom(scanner);
+            controller.start();
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan yang tidak terduga: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
