6264f6a2e0136ed22adbba3ed7499f520b034c64
zlvnt <zlvyynnnn@gmail.com>
2025-01-08 15:07:59 +0700

    Update Vehicle.java

--- showroom kendaraan/src/Vehicle.java
+++ showroom kendaraan/src/Vehicle.java
@@ -48,4 +48,15 @@ public class Vehicle {
     public String toString() {
         return "Brand: " + brand + ", Model: " + model + ", Harga: " + harga + ", Stock: " + stockQuantity;
     }
+
+    public static Vehicle buatKendaraan(String tipe, String brand, String model, long harga, int stok, String ekstra1, int ekstra2) {
+        if ("Car".equalsIgnoreCase(tipe)) {
+            return new Car(brand, model, harga, stok, ekstra1, ekstra2);
+        } else if ("Motorcycle".equalsIgnoreCase(tipe)) {
+            return new Motorcycle(brand, model, harga, stok, ekstra2, ekstra1);
+        }
+        throw new IllegalArgumentException("Tipe kendaraan tidak valid!");
+    }
+      
+    
 }
